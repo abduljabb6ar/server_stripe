8072bcb41a267afa3efa4c55627ad3282bb867e1
abduljabbar <abduljab6ar@gmail.com>
2024-05-23 23:12:53 +0300

    التغيير الى وضع التجريبي

--- index.js
+++ index.js
@@ -14,33 +14,53 @@ app.use(
     })
 );
 app.post('/create-checkout-session', async (req, res) => {
-    try {
-        // const price = req.body.price;
+    const session = await stripe.checkout.sessions.create({
+      line_items: [{
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: 'T-shirt',
+          },
+          unit_amount: 2000,
+        },
+        quantity: 1,
+      }],
+      mode: 'payment',
+      ui_mode: 'embedded',
+      return_url: 'https://example.com/checkout/return?session_id={CHECKOUT_SESSION_ID}'
+    });
+  
+    res.send({clientSecret: session.client_secret});
+  });
+// app.post('/create-checkout-session', async (req, res) => {
+//     try {
+//         // const price = req.body.price;
 
-        const session = await stripe.checkout.sessions.create({
-            line_items: [{
-                price_data: {
-                    currency: 'usd',
-                    product_data: {
-                        name: 'عسل سدر طبيعي',
-                    },
-                    unit_amount: 10 * 50 // تحويل السعر إلى سنتات
-                },
-                quantity: 1
-            }],
-            mode: 'payment',
-            success_url: 'https://ghidhaalruwh.netlify.app/complate',
-            cancel_url: 'https://ghidhaalruwh.netlify.app/cancel'
-        });
+//         const session = await stripe.checkout.sessions.create({
+//             line_items: [{
+//                 price_data: {
+//                     currency: 'usd',
+//                     product_data: {
+//                         name: 'عسل سدر طبيعي',
+//                     },
+//                     unit_amount: 10 * 50 // تحويل السعر إلى سنتات
+//                 },
+//                 quantity: 1
+//             }],
+//             mode: 'payment',
+//             ui_mode: 'embedded',
+//             success_url: 'https://ghidhaalruwh.netlify.app/complate',
+//             cancel_url: 'https://ghidhaalruwh.netlify.app/cancel'
+//         });
 
 
-      res.redirect(session.url); // توجيه المستخدم إلى صفحة الدفع في Stripe
+//       res.redirect(session.url); // توجيه المستخدم إلى صفحة الدفع في Stripe
 
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).send('Error occurred');
-    }
-});
+//     } catch (error) {
+//         console.error('Error:', error);
+//         res.status(500).send('Error occurred');
+//     }
+// });
 app.get('/complate',async(req,res)=>{
     try{
       await  paypal.capturpayment(req.query.token)
