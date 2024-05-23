abb566b8d4735217c82bd7bf8caef15a08c2b260
abduljabbar <abduljab6ar@gmail.com>
2024-05-23 16:04:37 +0300

    server stripe

---
+++ index.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+var express=require('express');
+const stripe=require('stripe')(process.env.STRIPE_SECRIT)
+var app=express();
+app.set('view engine','ejs');
+app.get('/',(req,res)=>{
+res.render('index')
+});
+app.post('/checkout', async(req,res)=>{
+    const sessstion=await stripe.checkout.sessions.create({
+        line_items:[
+            {
+                price_data:{
+                    currency:'usd',
+                    product_data:{
+                        name:'عسل سدر طبيعي',
+                    },
+                    unit_amount:1*50
+                },
+                quantity:1
+            }
+        ],
+        mode:'payment',
+        success_url:'http://localhost:8000/complate',
+        cancel_url:'http://localhost:8000/cancel'
+    })
+
+    // console.log(sessstion)
+    res.redirect(sessstion.url)
+}) 
+app.get('/complate',async(req,res)=>{
+    try{
+      await  paypal.capturpayment(req.query.token)
+     res.send('secessful')
+
+    }catch(error){
+        res.send('error : '+error)
+    }
+})
+app.get('/cancel',(rez,res)=>{
+    res.redirect('/')
+    })
+app.listen(8000,(req,res)=>{console.log('that is server 8000');});
